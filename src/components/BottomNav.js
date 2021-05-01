05eafa1f05b6453d4bf1c0556488975688f52c6e
ardiancok <ardiansyahparwanto@gmail.com>
2021-05-01 10:52:09 +0700

    first commit

---
+++ src/components/BottomNav.js
@@ -0,0 +1,37 @@
+import React from "react";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FolderIcon from "@material-ui/icons/Folder";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+function BottomNav() {
+  return (
+    <div className="BottomNav">
+      <BottomNavigation value={value}>
+        <BottomNavigationAction
+          label="Recents"
+          value="recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOnIcon />}
+        />
+        <BottomNavigationAction
+          label="Folder"
+          value="folder"
+          icon={<FolderIcon />}
+        />
+      </BottomNavigation>
+    </div>
+  );
+}
+
+export default BottomNav;
